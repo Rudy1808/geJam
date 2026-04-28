a1456799599e157e26c41758f5ea611b5331c5eb
Blazo810 <mocny.blazer@gmail.com>
2026-04-28 09:59:38 +0200

    TowerAtack

--- Assets/Scripts/Tower/TowerAttack.cs
+++ Assets/Scripts/Tower/TowerAttack.cs
@@ -1,9 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerAttack : MonoBehaviour
 {
     public int damage;
     public int range;
-    public int cost;
     public float fireRate;
+
+    public Queue<Transform> targetQueue = new Queue<Transform>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.LayerToName("Name"))
+    }
 }
