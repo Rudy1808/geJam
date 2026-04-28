ec161bf9f83d993700872f13ccedb8cad18a2ad6
Rudy1808 <chabasinskimarcel796@gmail.com>
2026-04-28 11:43:31 +0200

    powiino być git

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -1,150 +1,26 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
-public class TowerAttack : MonoBehaviour
+
+public class Tower : MonoBehaviour
 {
-    public int damage;
-    public int range = 6;
-    public float fireRate;
+    [SerializeField] private TowerUpgradeStats[] levels;
+    int currentLevel = 0;
 
-    bool isFacingRight;
+    public TowerUpgradeStats CurrentData => levels[currentLevel];
 
-    CircleCollider2D col;
-
-    public PriorityQueue<Transform> targetList = new PriorityQueue<Transform>();
-
-    private void Update()
+    public void Upgrade()
     {
-        Atack();
-    }
-    private void Awake()
-    {
-        col = GetComponent<CircleCollider2D>();
-        col.isTrigger = true;
-        col.radius = range;
+        if (currentLevel > levels.Length) return;
+        currentLevel++;
+        ApplyLevel();
+
     }
 
-    void Atack()
+    private void ApplyLevel()
     {
-        if (targetList.Count == 0) return;
-        Rotate();
-        Shoot(targetList.Peek());
-    }
-    void Rotate()
-    {
-        float distance = targetList.Peek().position.x - transform.position.x;
-        if ((isFacingRight && distance < 0) || (!isFacingRight && distance > 0))
-        {
-            isFacingRight = !isFacingRight;
-            transform.localScale = transform.localScale * -1;
-        }
-    }
-
-    void Shoot(Transform target)
-    {
-        Debug.Log("Strza³");
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemies"))
-        {
-            targetList.Enqueue(collision.transform, 1000-targetList.Peek().position.x);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemies"))
-        {
-            targetList.Dequeue();
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, range);
-    }
-}
-
-public class PriorityQueue<T>
-{
-    private List<(T item, float priority)> heap = new List<(T, float)>();
-
-    public int Count => heap.Count;
-
-    public void Enqueue(T item, float priority)
-    {
-        heap.Add((item, priority));
-        HeapifyUp(heap.Count - 1);
-    }
-
-    public T Dequeue()
-    {
-        if (heap.Count == 0)
-            throw new InvalidOperationException("Queue is empty");
-
-        T root = heap[0].item;
-
-        heap[0] = heap[heap.Count - 1];
-        heap.RemoveAt(heap.Count - 1);
-
-        HeapifyDown(0);
-
-        return root;
-    }
-
-    public T Peek()
-    {
-        if (heap.Count == 0)
-            throw new InvalidOperationException("Queue is empty");
-
-        return heap[0].item;
-    }
-
-    private void HeapifyUp(int i)
-    {
-        while (i > 0)
-        {
-            int parent = (i - 1) / 2;
-
-            if (heap[i].priority >= heap[parent].priority)
-                break;
-
-            Swap(i, parent);
-            i = parent;
-        }
-    }
-
-    private void HeapifyDown(int i)
-    {
-        int lastIndex = heap.Count - 1;
-
-        while (true)
-        {
-            int left = i * 2 + 1;
-            int right = i * 2 + 2;
-            int smallest = i;
-
-            if (left <= lastIndex && heap[left].priority < heap[smallest].priority)
-                smallest = left;
-
-            if (right <= lastIndex && heap[right].priority < heap[smallest].priority)
-                smallest = right;
-
-            if (smallest == i)
-                break;
-
-            Swap(i, smallest);
-            i = smallest;
-        }
-    }
-
-    private void Swap(int a, int b)
-    {
-        (heap[a], heap[b]) = (heap[b], heap[a]);
+        //Tower Atack
+        GetComponent<TowerAttack>().damage = CurrentData.damage;
+        GetComponent<TowerAttack>().range = CurrentData.range;
+        GetComponent<TowerAttack>().fireRate = CurrentData.fireRate;
     }
 }
